a15b66667dcceda2a939dc9ffed18e7ab5dda2f8
Abdul Shakoor <abduldev1786@gmail.com>
2024-04-12 21:35:58 +0500

    register and login controller create

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,8 @@ conf();
 const _config = {
     port: process.env.PORT,
     db_url:process.env.DB_URL,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    JWT_SECRET: process.env.JWT_SECRET
 }
 
 export const config = Object.freeze(_config);
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,15 @@
 import {Request,Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import { UserModel } from "../models/users.model";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
+
+
+
+// register controller
+// @access public
+// @route POST /api/users/register
 
 
 export const registerUser = async (req:Request, res:Response, next:NextFunction) => {
@@ -8,33 +17,98 @@ export const registerUser = async (req:Request, res:Response, next:NextFunction)
     const {name,email,password} = req.body;
 
     try {     
-    // validation
+    //@ validation
     if(!name || !email || !password){
         const error = createHttpError(400, "All fields are required.");
        return next(error);
     };
-    // check if user exists
+    //@ check if user exists
     const user = await UserModel.findOne({email});
     if(user){
         const error = createHttpError(400, "User already exists.");
         return next(error);
     };
-    // create user
+
+    //@ hashed password
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    
+    //@ create user
     const newUser = new UserModel({
         name,
         email,
-        password
+        password: hashedPassword
     });
-    // save user
+
+    //@ saving user in database
     const savedUser = await newUser.save();
+    
+    //@ token generation 
+    const token = jwt.sign({
+        _id: savedUser._id,
+    }, config.JWT_SECRET as string, {
+        expiresIn: "7d"
+    });
 
         res.status(201).json({
             status:"success",
             message:"Register successfully",
-            user:savedUser
-        })
+            user:savedUser.name,
+            accessToken:token
+        });
     } catch (error) {
-        next(error);
+       return next(error);
     }
 
-}
+};
+
+
+
+//@ login controller
+// @ access public
+// @ route POST /api/users/login
+
+
+export const loginUser = async (req:Request, res:Response, next:NextFunction) => {
+
+    const {email,password} = req.body;
+
+    try {     
+    //@ validation
+    if(!email || !password){
+        const error = createHttpError(400, "All fields are required.");
+       return next(error);
+    };
+
+    //@ check if user doesn't exists
+    const user = await UserModel.findOne({email});
+    if(!user){
+        const error = createHttpError(401, "User doesn't exists.");
+        return next(error);
+    };
+    
+    //@ verifying hashed password
+    const comparePassword = bcrypt.compareSync(password, user.password);
+    
+    if(!comparePassword){
+        const error = createHttpError(401, "email or password doesn't match.");
+        return next(error);
+    };
+    
+    //@access token generation 
+    const token = jwt.sign({
+        _id: user._id,
+    }, config.JWT_SECRET as string, {
+        expiresIn: "7d"
+    });
+
+        res.status(200).json({
+            status:"success",
+            message:"Login successfully",
+            user:user.name,
+            accessToken:token
+        });
+    } catch (error) {
+       return next(error);
+    }
+
+};
